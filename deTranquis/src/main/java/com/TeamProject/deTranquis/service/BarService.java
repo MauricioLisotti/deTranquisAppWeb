92328fcf2d7fc26ed433f987351da26c269035f3
mauricio <94354882+MauricioLisotti@users.noreply.github.com>
2022-05-27 13:23:43 +0200

    Add files via upload

---
+++ deTranquis/src/main/java/com/TeamProject/deTranquis/service/BarService.java
@@ -0,0 +1,19 @@
+package com.TeamProject.deTranquis.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.TeamProject.deTranquis.model.Bar;
+
+public interface BarService {
+
+    public List<Bar> findAllBars();
+
+    public Optional<Bar> findBarById(Long id);
+
+    public Bar saveBar(Bar barNew);
+
+    public String deleteBar(Long id);
+
+    public String updateBar(Bar barNew);
+}
